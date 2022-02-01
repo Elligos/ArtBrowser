bdf54485dbeb0c35953e246e4d84cc1b6b5f2338
Dmitriy Prakapovich <elligos@rambler.ru>
2022-02-01 23:01:06 +0300

    Made transitions between login and registration fragments by the click of a message instead of a button click.

--- app/src/main/java/com/example/deviantartviewer/FirstFragment.kt
+++ app/src/main/java/com/example/deviantartviewer/FirstFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 
 /**
@@ -24,7 +25,7 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_login).setOnClickListener {
+        view.findViewById<TextView>(R.id.textview_signup_message).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
--- app/src/main/java/com/example/deviantartviewer/SecondFragment.kt
+++ app/src/main/java/com/example/deviantartviewer/SecondFragment.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 
 /**
@@ -18,13 +18,13 @@ class SecondFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(R.layout.fragment_registration, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
+        view.findViewById<TextView>(R.id.textview_sign_in_message).setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
     }
